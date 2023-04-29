275e92b478a7ec5bd1240d1ebbab0fbc74b5bb79
tlau10 <tim.laukemann@gmx.de>
2023-04-29 12:12:06 +0200

    improved code

--- home_scraper.py
+++ home_scraper.py
@@ -14,11 +14,13 @@ CITIES = ["Überlingen", "Meersburg", "Friedrichshafen", "Uhldingen", "Ueberling
 class AbstractScraper:
     """Abstract scraper class"""
 
-    def __init__(self, urls, class_):
+    def __init__(self, urls, class_, page_title, tag):
         self.urls = urls
         self.class_ = class_
         self.header = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0"}
         self.items = list()
+        self.page_title = page_title 
+        self.tag = tag
 
     def save_items(self):
         """save items to json file and eliminate duplicates beforehand"""
@@ -49,134 +51,69 @@ class AbstractScraper:
         text = str(tag)
         return True if not any(city in text for city in CITIES) else False 
 
-class ImmoweltScraper(AbstractScraper):
-    """Scraper class for immowelt"""
+class GeneralScraper(AbstractScraper):
+    """General Scraper class"""
 
-    def __init__(self, urls, class_):
-        AbstractScraper.__init__(self, urls, class_)
+    def __init__(self, urls, class_, page_title, tag):
+        AbstractScraper.__init__(self, urls, class_, page_title, tag)
 
     def scrape(self):
         """start scraping"""
-
         for url in self.urls:
             print(url)
-            page = requests.get(url = url, headers = self.header)
-            soup = BeautifulSoup(page.content, "html.parser")
-            result = soup.find_all("a", class_ = self.class_)
+            page = self.make_request(request_url = url)
+            tags = self.parse_html(html = page, tag=self.tag)
 
-            for a_tag in result:
+            for a_tag in tags:
                 # skip <a> tags with wrong city
                 if self.no_valid_city(tag = a_tag):
                     continue
 
-                link = a_tag["href"]
+                a_tag = a_tag.find("a") if self.page_title == "immonet" or self.page_title == "wohnungsboerse" or self.page_title == "immobilo" else a_tag
+                link_url = self.extract_url(a_tag = a_tag, page_title = self.page_title)
+                link = self.build_link(url = link_url, page_title = self.page_title)
+
                 self.items.append(link)
                 print(link)
 
         self.save_items()
+    
+    def make_request(self, request_url):
+        """
+        make http request to given url
+        @param request_url: url to request html content from
+        """
+        return requests.get(url = request_url, headers = self.header)
 
-class ImmonetScraper(AbstractScraper):
-    """Scraper class for immonet"""
+    def parse_html(self, html, tag):
+        """
+        parse html page to find given tags
+        @param html: html page
+        @param tag: tags to find in html, eg.: <div>
+        """
+        soup = BeautifulSoup(html.content, "html.parser")
+        return soup.find_all(tag, class_ = self.class_)
 
-    def __init__(self, urls, class_):
-        AbstractScraper.__init__(self, urls, class_)
+    def extract_url(self, a_tag, page_title):
+        """
+        Extract link from given a_tag by extracting specific html class, eg.: href
+        @param a_tag: <a> tag from search result
+        @param page_title: title of page search result belongs to, eg.: ebay; is set on class initialization
+        """
+        html_class = "data-href" if page_title == "ebay" else "href"
+        return str(a_tag[html_class])
 
-    def scrape(self):
-        """start scraping"""
-        for url in self.urls:
-            print(url)
-            page = requests.get(url = url, headers = self.header)
-            soup = BeautifulSoup(page.content, "html.parser")
-            result = soup.find_all("div", class_ = self.class_)
-
-            for div_tag in result:
-                # skip <div> tags with wrong city
-                if self.no_valid_city(tag = div_tag):
-                    continue
-
-                a_tag = div_tag.find("a")
-                link = "https://www.immonet.de"+str(a_tag["href"])
-                self.items.append(link)
-                print(link)
-
-        self.save_items()
-
-class EbayScraper(AbstractScraper):
-    """Scraper class for ebay"""
-
-    def __init__(self, urls, class_):
-        AbstractScraper.__init__(self, urls, class_)
-
-    def scrape(self):
-        """start scraping"""
-        for url in self.urls:
-            print(url)
-            page = requests.get(url = url, headers = self.header)
-            soup = BeautifulSoup(page.content, "html.parser")
-            result = soup.find_all("article", class_ = self.class_)
-
-            for article_tag in result:
-                # skip <article> tags with wrong city
-                if self.no_valid_city(tag = article_tag):
-                    continue
-
-                link = "https://www.ebay-kleinanzeigen.de"+str(article_tag["data-href"])
-                self.items.append(link)
-                print(link)
-
-        self.save_items()
-
-class WohnungsboerseScraper(AbstractScraper):
-    """Scraper class for wohnungsboerse"""
-
-    def __init__(self, urls, class_):
-        AbstractScraper.__init__(self, urls, class_)
-
-    def scrape(self):
-        """start scraping"""
-        for url in self.urls:
-            print(url)
-            page = requests.get(url = url, headers = self.header)
-            soup = BeautifulSoup(page.content, "html.parser")
-            result = soup.find_all("div", class_ = self.class_)
-
-            for div_tag in result:
-                # skip <div> tags with wrong city
-                if self.no_valid_city(tag = div_tag):
-                    continue
-                
-                a_tag = div_tag.find("a")
-                link = str(a_tag["href"])
-                self.items.append(link)
-                print(link)
-
-        self.save_items()
-
-class ImmobiloScraper(AbstractScraper):
-    """Scraper class for immobilo"""
-
-    def __init__(self, urls, class_):
-        AbstractScraper.__init__(self, urls, class_)
-
-    def scrape(self):
-        """start scraping"""
-        for url in self.urls:
-            print(url)
-            page = requests.get(url = url, headers = self.header)
-            soup = BeautifulSoup(page.content, "html.parser")
-            result = soup.find_all("div", class_ = self.class_)
-
-            for div_tag in result:
-                # skip <div> tags with wrong city
-                if self.no_valid_city(tag = div_tag):
-                    continue
-                
-                a_tag = div_tag.find("a")
-                link = "https://www.immobilo.de"+str(a_tag["href"])
-                self.items.append(link)
-                print(link)
-
-        self.save_items()
+    def build_link(self, url, page_title):
+        """
+        Build link from given url, adding a prefix depending on page
+        @param url: url from of search result
+        @param page_title: title of page search result belongs to, eg.: ebay; is set on class initialization
+        """
+        prefix = str()
+        prefix = "https://www.immonet.de" if page_title == "immonet" else prefix
+        prefix = "https://www.ebay-kleinanzeigen.de" if page_title == "ebay" else prefix
+        prefix = "https://www.immobilo.de" if page_title == "immobilo" else prefix
+        return f"{prefix}{url}"
 
 def remove_from_whitelist(item):
     """remove item from whitelist"""
@@ -204,17 +141,18 @@ if __name__=="__main__":
 
     data = read_json_file(file_path = WEBSITE)
 
-    immowelt = ImmoweltScraper(urls = data["immowelt"]["urls"], class_ = data["immowelt"]["class"])
+    immowelt = GeneralScraper(urls = data["immowelt"]["urls"], class_ = data["immowelt"]["class"], page_title = "immowelt", tag = "a")
     immowelt.scrape()
 
-    immonet = ImmonetScraper(urls = data["immonet"]["urls"], class_ = data["immonet"]["class"])
+    immonet = GeneralScraper(urls = data["immonet"]["urls"], class_ = data["immonet"]["class"], page_title = "immonet", tag = "div")
     immonet.scrape()
 
-    ebay = EbayScraper(urls = data["ebay"]["urls"], class_ = data["ebay"]["class"])
+    ebay = GeneralScraper(urls = data["ebay"]["urls"], class_ = data["ebay"]["class"], page_title = "ebay", tag = "article")
     ebay.scrape()
 
-    wohnungsboerse = WohnungsboerseScraper(urls = data["wohnungsboerse"]["urls"], class_ = data["wohnungsboerse"]["class"])
+    wohnungsboerse = GeneralScraper(urls = data["wohnungsboerse"]["urls"], class_ = data["wohnungsboerse"]["class"], page_title = "wohnungsboerse", tag = "div")
     wohnungsboerse.scrape()
 
-    immobilo = ImmobiloScraper(urls = data["immobilo"]["urls"], class_ = data["immobilo"]["class"])
+    immobilo = GeneralScraper(urls = data["immobilo"]["urls"], class_ = data["immobilo"]["class"], page_title = "immobilo", tag = "div")
     immobilo.scrape()
+    
